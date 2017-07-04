24594a25360f9b57c071d6f6b107a4e818428e46
Haran Kumar <hkumar8200@gmail.com>
2017-07-04 17:52:04 +0000

    Update data to handle regional variants

--- server.js
+++ server.js
@@ -62,6 +62,10 @@ String.prototype.toProperCase = function () {
     return this.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()})
 }
 
+Array.prototype.toProperCase = function (){
+  return this.map((x) => x.toProperCase())
+}
+
 app.get("/userlangs/:prefix", function(request, response){
   response.set('Access-Control-Allow-Origin', '*')
   
